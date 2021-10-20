922c53fdd442c2ff6ce200b8a5094651f7118ae8
Sebastian Daschner <git@sebastian-daschner.de>
2021-10-20 15:58:18 +0200

    concurrency, scheduler, async explained

--- src/main/java/com/sebastian_daschner/workshops/ConcurrentResource.java
+++ src/main/java/com/sebastian_daschner/workshops/ConcurrentResource.java
@@ -1,2 +1,36 @@
-package com.sebastian_daschner.workshops;public class ConcurrentResource {
+package com.sebastian_daschner.workshops;
+
+import io.quarkus.runtime.Startup;
+import io.quarkus.scheduler.Scheduled;
+import org.eclipse.microprofile.context.ManagedExecutor;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.util.concurrent.locks.LockSupport;
+
+@Path("concurrent")
+@ApplicationScoped
+@Startup
+public class ConcurrentResource {
+
+    @Inject
+    ManagedExecutor executor;
+
+    @GET
+    public String execute() {
+        executor.execute(() -> {
+            System.out.println("processing ...");
+            LockSupport.parkNanos(2_000_000_000L);
+            System.out.println("done");
+        });
+        return "Hello";
+    }
+
+    @Scheduled(every = "3s")
+    public void executeScheduler() {
+        System.out.println("run in scheduler");
+    }
+
 }
